d0153f51405ab68c3ff9977c2ac7ea9cda59e508
Mads Højris Kristensen <cph-mk613@cphbusiness.dk>
2020-10-20 09:25:27 +0200

    added personDTO

---
+++ backend/src/main/java/dto/PersonDTO.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import java.util.List;
+import entities.*;
+
+/**
+ *
+ * @author madsa
+ */
+public class PersonDTO {
+
+    private Integer id;
+    private String name;
+    private String gender;
+    private String email;
+    private String birthday;
+    private List<Phone> phones;
+    private Address address;
+    private List<Hobby> hobbies;
+
+    public PersonDTO() {
+    }
+
+    public PersonDTO(String name, String gender, String email, String birthday, List<Phone> phones, Address address, List<Hobby> hobbies) {
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.birthday = birthday;
+        this.phones = phones;
+        this.address = address;
+        this.hobbies = hobbies;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Hobby> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<Hobby> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+}
