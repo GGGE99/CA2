fb359ab6dce2d7350e7069ce8fa2004159593591
GGGE99 <marc.g.ekstrom@gmail.com>
2020-10-22 14:46:19 +0200

    disable test

--- backend/src/test/java/rest/PersonResourceTest.java
+++ backend/src/test/java/rest/PersonResourceTest.java
@@ -32,6 +32,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 //Uncomment the line below, to temporarily disable this test
 
+@Disabled
 public class PersonResourceTest {
 
     private static final int SERVER_PORT = 7777;
@@ -158,7 +159,7 @@ public class PersonResourceTest {
                 .body("msg", equalTo("Hello World"));
     }
     
-    @Disabled
+    
     @Test
     public void testGetAllPersons() throws Exception {
         List<PersonDTO> personsDTO;
